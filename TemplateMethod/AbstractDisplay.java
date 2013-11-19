5248d06baefdf04b7e8d5a8f5790cbe62d232e3b
Lewuathe <lewuathe@me.com>
2013-11-19 22:00:32 +0900

    Template method

---
+++ TemplateMethod/AbstractDisplay.java
@@ -0,0 +1,13 @@
+public abstract class AbstractDisplay {
+	public abstract void open();
+	public abstract void print();
+	public abstract void close();
+
+	public final void display() {
+		open();
+		for (int i = 0; i < 5; i++) {
+			print();
+		}
+		close();
+	}
+}
---
+++ TemplateMethod/CharDisplay.java
@@ -0,0 +1,18 @@
+public class CharDisplay extends AbstractDisplay {
+	private char ch;
+	public CharDisplay(char ch) {
+		this.ch = ch;
+	}
+	@Override
+	public void open() {
+		System.out.print("<<");
+	}
+	@Override
+	public void print() {
+		System.out.print(ch);
+	}
+	@Override
+	public void close() {
+		System.out.println(">>");
+	}
+}
