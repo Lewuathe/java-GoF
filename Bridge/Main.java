df68ff49b5e5b68b122d68fd199015ed5217916d
Lewuathe <lewuathe@me.com>
2013-11-30 15:43:23 +0900

    Birdge pattern

---
+++ Bridge/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		Display d1 = new Display(new StringDisplayImpl("Hello, JAPAN"));
+		Display d2 = new CountDisplay(new StringDisplayImpl("Hello, RUSSIA"));
+		CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe"));
+
+		d1.display();
+		d2.display();
+		d3.multiDisplay(3);
+	}
+}
+		
